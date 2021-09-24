95983468a73fdb532d406fe1bf8560196dd02bd2
John Payne <john@iboard.ca>
2021-09-24 15:41:41 -0700

    added api call

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Search from './search';
+const API_URL = "http://localhost:3001/get_from_omdb"
 
-function App() {
-  return (
-	<Search />
-  );
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			results: []
+		}
+	}
+	
+	componentDidMount() {
+		fetch(API_URL + '?s=star+trek' + '&type=series')
+			.then(data => data.json())
+			.then(results => this.setState( {results} ));
+	}
+
+	render() {
+		console.log(this.state.results);
+		return (
+			<div className="App">
+				<Search />
+			</div>
+		);
+	}
 }
 
 export default App;
